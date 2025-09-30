7ed3da6c1b5cb9f28515a0c98cb78089fc6fe6ec
kiruh <kiruha2387@gmail.com>
2025-09-30 15:55:27 +0300

    html optimize

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'catalog/home.html', context)
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product": product}
+    return render(request, 'catalog/product.html', context)
